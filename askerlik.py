585d8296ba8bea09fe1c5e80450ab9543c9afe9d
Ali Beyaz <symbuzzer@users.noreply.github.com>
2022-11-06 04:31:14 +0300

    Update askerlik.py

--- askerlik.py
+++ askerlik.py
@@ -9,7 +9,7 @@ if cinsiyet=="e":
     else:
         print("Lütfen yaş olarak bir sayı girin")
 if cinsiyet=="k":
-        print("MUAF")
+       print("MUAF")
 if cinsiyet=="":
     print("Lütfen bir cinsiyet girin")
 if cinsiyet!="e" and cinsiyet!="k" and cinsiyet!="":
