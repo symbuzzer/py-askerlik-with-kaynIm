0ceb9e58949d6ec346ed1a384ef330b256bae8ce
Ali Beyaz <symbuzzer@users.noreply.github.com>
2022-11-06 04:15:17 +0300

    Add files via upload

---
+++ askerlik.py
@@ -0,0 +1,16 @@
+cinsiyet=input("Cinsiyet giriniz: ")
+if cinsiyet=="e":
+    yas=input("Yaşınızı giriniz: ")
+    if yas.isnumeric():
+        if yas>="20":
+            print("Askere gelmelisin")
+        else:
+            print("Erken")
+    else:
+        print("Lütfen yaş olarak bir sayı girin")
+if cinsiyet=="k":
+        print("MUAF")
+if cinsiyet=="":
+    print("Lütfen bir cinsiyet girin")
+if cinsiyet!="e" and cinsiyet!="k" and cinsiyet!="":
+    print("Cinsiyeti yanlış girdiniz")
