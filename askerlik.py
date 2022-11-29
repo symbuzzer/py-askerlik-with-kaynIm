47567facbd21400e865175bb2c825a8bb8ce4d04
Ali Beyaz <symbuzzer@users.noreply.github.com>
2022-11-29 13:03:33 +0300

    Update askerlik.py

--- askerlik.py
+++ askerlik.py
@@ -1,17 +1,27 @@
-cinsiyet=input("Cinsiyet giriniz (e/k): ")
-if cinsiyet=="e":
-    yas=input("Yaşınızı giriniz: ")
-    if yas.isnumeric():
-        if yas>="20":
-            print("Askere gelmelisin")
-        else:
-            print("Erken")
-    else:
-        print("Lütfen yaş olarak bir sayı girin")
-if cinsiyet=="k":
-       print("MUAF")
-if cinsiyet=="":
-    print("Lütfen bir cinsiyet girin")
-if cinsiyet!="e" and cinsiyet!="k" and cinsiyet!="":
-    print("Cinsiyeti yanlış girdiniz")
-input("Press Enter to continue...")
+import sys;
+
+while True:
+    cinsiyet=input("Cinsiyet giriniz (e/k): ")
+    if cinsiyet=="e":
+        while True:
+            try:
+                yas=int(input("Yaşınızı giriniz: "))
+                if yas <= 20:
+                    print("Erken")
+                else:
+                    print ("Askere gitmelisin")
+                input("Çıkmak için Enter tuşuna basın...")
+                sys.exit(0)
+            except ValueError:
+                print("Lütfen yaş olarak bir sayı girin")
+                continue
+    if cinsiyet=="k":
+        print("MUAF")
+        input("Çıkmak için Enter tuşuna basın...")
+        sys.exit(0)
+    if cinsiyet=="":
+        print("Lütfen bir cinsiyet girin")
+        continue
+    if cinsiyet!="e" and cinsiyet!="k" and cinsiyet!="":
+        print("Cinsiyeti yanlış girdiniz")
+        continue
